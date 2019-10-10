e79df7f4f664b9e562807ef01c11e344d6ca1372
ahaigh9877 <alastairhaigh@me.com>
2019-10-10 17:23:11 +0200

    Display elements and reset form

--- pages/detail-page.js
+++ pages/detail-page.js
@@ -13,12 +13,21 @@ function submitComment() {
 
     // adjust these new elements by adding the data from the form.
     h3.innerHTML = `${name} said:`;
-    p.innerHTML = msg;
+    // Puts quote-marks around the message.
+    p.innerHTML = `"${msg}"`;
 
     // add the stuff to the section called comment: "comment", h3(name) and p(msg)
     comment.classList.add("comment");
     comment.appendChild(h3);
     comment.appendChild(p);
+    // adds the css class "handwriting" to the p element so it's the right font.
+    p.classList.add("handwriting");
 
-    
+    // creating comment section element and adding the comment to it.
+    const commentSection = document.getElementById("comments");
+    commentSection.appendChild(comment);
+
+    // empty the form after comment submission
+    inputField.value = null;
+    textArea.value = null;
 }
